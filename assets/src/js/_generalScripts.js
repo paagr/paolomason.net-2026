7166754f0aaddc833205dd4b405d4c8e9c501bc3
Paolo Mason <mason.paolo@gmail.com>
2025-11-16 16:34:43 +0100

    Initial commit for SS theme

---
+++ assets/src/js/_generalScripts.js
@@ -0,0 +1,14 @@
+class General {
+	constructor() {
+		this.testVariable = 'script working';
+		this.init();
+	}
+
+	init() {
+		// for tests purposes only
+		// eslint-disable-next-line no-console
+		console.log(this.testVariable);
+	}
+}
+
+export default General;
